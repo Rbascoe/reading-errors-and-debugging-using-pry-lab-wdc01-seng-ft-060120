4d5e83cec46003736e8dcdf4c5b2c4fe3fd8cdf0
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-22 15:22:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,11 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-def snake_it_up("string")
-  if "string"[0] == "s"
-   10.to_s * "s" + "string"
+def snake_it_up(string)
+  if string[0] == "s"
+   "s" * 10 + string
   else
-    "string"
+    string
   end
 end
 
